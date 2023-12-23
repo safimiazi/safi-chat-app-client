15c6e6d62261c67a5eaf1874183a0a1b8745588f
safimiazi <mohibullamiazi@gmail.com>
2023-12-23 22:52:06 +0600

    typo added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { Box, useTheme } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
 import { Stack } from 'phosphor-react';
 import React from 'react';
 
@@ -9,7 +9,9 @@ const Contact = () => {
             <Stack sx={{ height: "100vh" }}>
                 <Box sx={{ boxShadow: "0px 0px 2px rgba(0,0,0, 0.25)", width: "100%", backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background }}>
 <Stack sx={{height: "100%", p:2}} direction="row" alignItems={"center"} justifyContent="space-between" spacing={3}>
-
+<Typography>
+    
+</Typography>
 </Stack>
 
                 </Box>
