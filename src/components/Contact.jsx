f7cefd794ab6bb6c9c2b10d7ea114e8ec9ee1193
safimiazi <mohibullamiazi@gmail.com>
2023-12-23 23:09:05 +0600

    stack done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,6 +24,9 @@ const Contact = () => {
                     </Stack>
                 </Box>
                 {/* body */}
+                <Stack>
+                    
+                </Stack>
             </Stack>
         </Box>
     );
