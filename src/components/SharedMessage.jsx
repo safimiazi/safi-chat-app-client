daa92b11527cc4ae67f72911ad4419310ba1ef30
safimiazi <mohibullamiazi@gmail.com>
2023-12-24 16:05:55 +0600

    change icon

--- src/components/SharedMessage.jsx
+++ src/components/SharedMessage.jsx
@@ -11,15 +11,16 @@ const SharedMessage = () => {
         <Box sx={{ width: 320, height: "100vh" }}>
             <Stack sx={{ height: "100%" }}>
                 <Box sx={{ boxShadow: "0px 0px 2px rgba(0,0,0, 0.25)", width: "100%", backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background }}>
-                    <Stack sx={{ height: "100%", p: 2 }} direction="row" alignItems={"center"} justifyContent="space-between" spacing={3}>
-                        <Typography variant='subtitle2'>
-                            Contact Info
-                        </Typography>
+                    <Stack sx={{ height: "100%", p: 2 }} direction="row" alignItems={"center"} spacing={3}>
                         <IconButton onClick={() => {
                             dispatch(UpdateSidebarType("CONTACT"))
                         }}>
                             <X />
                         </IconButton>
+                        <Typography variant='subtitle2'>
+                            Shared Messages
+                        </Typography>
+
                     </Stack>
                 </Box>
             </Stack>
