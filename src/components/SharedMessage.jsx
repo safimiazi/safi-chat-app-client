ece21ef26960a9b78e10e88e2a86376a7d9d2ff4
safimiazi <mohibullamiazi@gmail.com>
2023-12-24 16:02:15 +0600

    share button added

--- src/components/SharedMessage.jsx
+++ src/components/SharedMessage.jsx
@@ -1,12 +1,28 @@
-import { Box, Stack } from '@mui/material';
+import { Box, IconButton, Stack, Typography, useTheme } from '@mui/material';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { UpdateSidebarType } from '../redux/slices/Apps';
+import { X } from 'phosphor-react';
 
 const SharedMessage = () => {
+    const theme = useTheme()
+    const dispatch = useDispatch()
     return (
-        <Box sx={{width: 320, height: "100vh"}}>
-<Stack sx={{height: "100%"}}>
-
-</Stack>
+        <Box sx={{ width: 320, height: "100vh" }}>
+            <Stack sx={{ height: "100%" }}>
+                <Box sx={{ boxShadow: "0px 0px 2px rgba(0,0,0, 0.25)", width: "100%", backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background }}>
+                    <Stack sx={{ height: "100%", p: 2 }} direction="row" alignItems={"center"} justifyContent="space-between" spacing={3}>
+                        <Typography variant='subtitle2'>
+                            Contact Info
+                        </Typography>
+                        <IconButton onClick={() => {
+                            dispatch(UpdateSidebarType("CONTACT"))
+                        }}>
+                            <X />
+                        </IconButton>
+                    </Stack>
+                </Box>
+            </Stack>
         </Box>
     );
 };
