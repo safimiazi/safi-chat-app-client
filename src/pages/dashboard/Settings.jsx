b5b495e4d506aca91101930e504041e7a6afeed8
safimiazi <mohibullamiazi@gmail.com>
2023-12-25 14:06:45 +0600

    right box set up

--- src/pages/dashboard/Settings.jsx
+++ src/pages/dashboard/Settings.jsx
@@ -7,9 +7,12 @@ const Settings = () => {
           <Stack direction={"row"} sx={{width: "100%"}}>
 {/* left pannel */}
 <Box>
-    
+
 </Box>
 {/* right pannel */}
+<Box>
+    
+</Box>
           </Stack>
         </>
     );
