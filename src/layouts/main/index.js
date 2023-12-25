ed0757c0a6267faae4148f40f2acad1472740bcf
safimiazi <mohibullamiazi@gmail.com>
2023-12-25 19:20:47 +0600

    logo setup

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -1,18 +1,18 @@
 import { Container, Stack } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
-
+import logo from "../../assets/Images/logo.ico"
 const MainLayout = () => {
   return (
     <>
-    <Container>
-      <Stack spacing={5}>
-<Stack sx={{width:"100%"}} direction={"column"} alignItems={"center"}>
+      <Container>
+        <Stack spacing={5}>
+          <Stack sx={{ width: "100%" }} direction={"column"} alignItems={"center"}>
+            <img style={{ height: 120, width: 120 }} src={logo} alt="logo" />
+          </Stack>
+        </Stack>
 
-</Stack>
-      </Stack>
-
-      <Outlet />
+        <Outlet />
       </Container>
     </>
   );
