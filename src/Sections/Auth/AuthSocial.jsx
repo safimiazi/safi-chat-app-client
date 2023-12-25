32afba964640cf3b773c9a0aad92cd38cb1f6eb9
safimiazi <mohibullamiazi@gmail.com>
2023-12-25 20:28:21 +0600

    add mxwidth in the container

--- src/Sections/Auth/AuthSocial.jsx
+++ src/Sections/Auth/AuthSocial.jsx
@@ -1,9 +1,10 @@
+import { Divider } from '@mui/material';
 import React from 'react';
 
 const AuthSocial = () => {
     return (
         <div>
-            
+            <Divider>OR</Divider>
         </div>
     );
 };
--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -5,7 +5,7 @@ import logo from "../../assets/Images/logo.ico"
 const MainLayout = () => {
   return (
     <>
-      <Container>
+      <Container maxWidth={"sm"}>
         <Stack spacing={5}>
           <Stack sx={{ width: "100%" }} direction={"column"} alignItems={"center"}>
             <img style={{ height: 120, width: 120 }} src={logo} alt="logo" />
