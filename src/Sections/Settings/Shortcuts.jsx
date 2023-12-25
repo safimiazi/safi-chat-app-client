05c868992a84f0fcb10d2c65e0d7d4fef93efa29
safimiazi <mohibullamiazi@gmail.com>
2023-12-25 16:32:10 +0600

    dialog install

--- src/Sections/Settings/Shortcuts.jsx
+++ src/Sections/Settings/Shortcuts.jsx
@@ -1,10 +1,29 @@
+import { Dialog, Slide } from '@mui/material';
 import React from 'react';
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
+const Shortcuts = ({ open, handleClose }) => {
+    const [open, setOpen] = React.useState(false);
 
-const Shortcuts = () => {
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
     return (
-        <div>
-            
-        </div>
+        <>
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-describedby="alert-dialog-slide-description"
+            >
+            </Dialog>
+        </>
     );
 };
 
