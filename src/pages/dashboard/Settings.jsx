fd87302b519f7ab07bf95e03cfaff9401e6d3dcf
safimiazi <mohibullamiazi@gmail.com>
2023-12-25 14:03:14 +0600

    direction setup

--- src/pages/dashboard/Settings.jsx
+++ src/pages/dashboard/Settings.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Settings = () => {
     return (
         <>
-          <Stack>
-            
+          <Stack direction={"row"}>
+
           </Stack>
         </>
     );
