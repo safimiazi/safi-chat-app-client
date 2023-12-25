94de2474ccb79547d3c8cb7c17f3a9587bac3f73
safimiazi <mohibullamiazi@gmail.com>
2023-12-25 15:00:13 +0600

    boxshadow added

--- src/pages/dashboard/Settings.jsx
+++ src/pages/dashboard/Settings.jsx
@@ -7,7 +7,7 @@ const Settings = () => {
         <>
           <Stack direction={"row"} sx={{width: "100%"}}>
 {/* left pannel */}
-<Box sx={{overflowY: "auto",height:"100vh",width: 320, backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background}}>
+<Box sx={{overflowY: "auto",height:"100vh",width: 320, backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background, boxShadow:"0px 0px 2px rgba(0,0,0, 0.25)"}}>
 
 </Box>
 {/* right pannel */}
