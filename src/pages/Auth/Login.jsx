1c71e9bcb2f3377fa946033cf61e744e027b73da
safimiazi <mohibullamiazi@gmail.com>
2023-12-25 19:54:30 +0600

    stap system impletment

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,10 +1,13 @@
+import { Stack } from '@mui/material';
 import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            login
-        </div>
+        <>
+            <Stack spacing={2} sx={{mb: 5, position: "relative"}}>
+
+            </Stack>
+        </>
     );
 };
 
