affba2da2aaba7f8e6b1e9533069c5c4897f1359
safimiazi <mohibullamiazi@gmail.com>
2023-12-25 17:48:06 +0600

    grid added

--- src/Sections/Settings/Shortcuts.jsx
+++ src/Sections/Settings/Shortcuts.jsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogContent, DialogTitle, Grid, Slide } from '@mui/material';
+import { Dialog, DialogContent, DialogTitle, Grid, Slide, Stack } from '@mui/material';
 import React from 'react';
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
@@ -105,7 +105,14 @@ const list = [
                 <DialogTitle>Keyboard Shortcuts</DialogTitle>
                 <DialogContent sx={{ mt: 4 }}>
                     <Grid container spacing={3}>
+{list.map(({key, title, combination})=> <>
+<Grid item xs={6}>
+    <Stack>
 
+    </Stack>
+</Grid>
+</>
+)}
                     </Grid>
                 </DialogContent>
             </Dialog>
