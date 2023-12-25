7ae817301a92c5b97eb4e28e13530e1e67058440
safimiazi <mohibullamiazi@gmail.com>
2023-12-25 20:03:21 +0600

    Link added from mui

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,11 +1,19 @@
-import { Stack } from '@mui/material';
+import { Link, Stack, Typography } from '@mui/material';
+import { Link as RouterLink } from "react-router-dom"
 import React from 'react';
 
 const Login = () => {
     return (
         <>
-            <Stack spacing={2} sx={{mb: 5, position: "relative"}}>
+            <Stack spacing={1} sx={{ mb: 5, position: "relative" }}>
+                <Typography variant="h4">Login to Safichat</Typography>
+                <Stack direction={"row"} spacing={0.5}>
+                    <Typography variant='body2'>New User?</Typography>
+                    <Link to={"/auth/register"} component={RouterLink} variant="subtitle2">
+                        Create an account
 
+                    </Link>
+                </Stack>
             </Stack>
         </>
     );
