30c9a7da40682e173b4d9e18e191ac53be306dc3
safimiazi <mohibullamiazi@gmail.com>
2023-12-25 20:40:17 +0600

    three logo added

--- src/Sections/Auth/AuthSocial.jsx
+++ src/Sections/Auth/AuthSocial.jsx
@@ -1,10 +1,22 @@
-import { Divider } from '@mui/material';
+import { Divider, IconButton, Stack } from '@mui/material';
+import { GithubLogo, GoogleLogo, TwitterLogo } from 'phosphor-react';
 import React from 'react';
 
 const AuthSocial = () => {
     return (
         <div>
             <Divider sx={{my:2.5, typography: "overline", color: "text.disabled", "&::before, ::after": {borderTopStyle: "dashed"}}}>OR</Divider>
+        <Stack direction={"row"} justifyContent={"center"} spacing={2}>
+<IconButton>
+    <GoogleLogo color='#DF3E30'/>
+</IconButton>
+<IconButton>
+    <GithubLogo />
+</IconButton>
+<IconButton>
+    <TwitterLogo color='#1C9CEA'/>
+</IconButton>
+        </Stack>
         </div>
     );
 };
