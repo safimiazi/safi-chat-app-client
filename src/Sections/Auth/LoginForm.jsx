1aac0b2a6a80947ef17c4450883eb8a4884adb12
safimiazi <mohibullamiazi@gmail.com>
2023-12-25 22:37:41 +0600

    error setup

--- src/Sections/Auth/LoginForm.jsx
+++ src/Sections/Auth/LoginForm.jsx
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
 import FormProvider from '../../components/hook-form/FormProvider';
-
+import * as Yup from 'yup';
+import {useForm} from 'react-hook-form'
+import {YupResolver} from "@hookform/resolvers"
 const LoginForm = () => {
     const [showPassword, setShowPassword] = useState(false);
 
-    const LoginSchema =
+    const LoginSchema = Yup.object().shape({
+        email: Yup.string().required("Email is required").email("Email must be a valid email address"),
+        password: Yup.string().required("password is required"),
+    });
+
+    const defaultValues = {
+        email: "demo@safichat.com",
+        password: "demo1234"
+    }
+
+    const methods = useForm({
+        resolver: YupResolver(LoginForm),
+        defaultValues,
+    });
+
+    const {reset, setError, handleSubmit, formState:{errors, isSubmitting, isSubmittingSuccessful}} = methods;
+    
+    
     return (
         <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
             
