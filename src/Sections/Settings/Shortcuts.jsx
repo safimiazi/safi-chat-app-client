33c9d0b6ce59842f4f4c3bfec4ba922aa17ff99a
safimiazi <mohibullamiazi@gmail.com>
2023-12-25 17:45:34 +0600

    list added

--- src/Sections/Settings/Shortcuts.jsx
+++ src/Sections/Settings/Shortcuts.jsx
@@ -2,10 +2,96 @@ import { Dialog, DialogContent, DialogTitle, Grid, Slide } from '@mui/material';
 import React from 'react';
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
-  });
+});
 const Shortcuts = ({ open, handleClose }) => {
 
-
+const list = [
+    {
+        key: 0,
+        title: "Mark as Unread",
+        combination: ["Cmd", "Shift","U"]
+    },
+    {
+        key: 1,
+        title: "Mute",
+        combination: ["Cmd", "Shift","M"]
+    },
+    {
+        key: 2,
+        title: "Archive Chat",
+        combination: ["Cmd", "Shift","E"]
+    },
+    {
+        key: 3,
+        title: "Delete Chat",
+        combination: ["Cmd", "Shift","D"]
+    },
+    {
+        key: 4,
+        title: "Pin Chat",
+        combination: ["Cmd", "Shift","P"]
+    },
+    {
+        key: 5,
+        title: "Search",
+        combination: ["Cmd","F"]
+    },
+    {
+        key: 6,
+        title: "Search Chat",
+        combination: ["Cmd", "Shift","F"]
+    },
+    {
+        key: 7,
+        title: "Next Chat",
+        combination: ["Cmd","N"]
+    },
+    {
+        key: 8,
+        title: "Next Step",
+        combination: ["Ctrl", "Tab"]
+    },
+    {
+        key: 9,
+        title: "Previous Step",
+        combination: ["Ctrl", "Shift","Tab"]
+    },
+    {
+        key: 10,
+        title: "New Group",
+        combination: ["Cmd", "Shift","N"]
+    },
+    {
+        key: 11,
+        title: "Profile & About",
+        combination: ["Cmd","P"]
+    },
+    {
+        key: 12,
+        title: "Increase speed of voice message",
+        combination: ["Shift","."]
+    },
+    {
+        key: 13,
+        title: "Decrease speed of voice message",
+        combination: ["Shift",","]
+    },
+    {
+        key: 14,
+        title: "Settings",
+        combination: ["Shift","5"]
+    },
+    {
+        key: 15,
+        title: "Emoji Panel",
+        combination: ["Cmd","E"]
+    },
+    {
+        key: 16,
+        title: "Sticker Panel",
+        combination: ["Cmd","5"]
+    },
+]
 
     return (
         <>
@@ -14,13 +100,13 @@ const Shortcuts = ({ open, handleClose }) => {
                 TransitionComponent={Transition}
                 keepMounted
                 onClose={handleClose}
-                fullWidth maxWidth="md" sx={{p:4}}
+                fullWidth maxWidth="md" sx={{ p: 4 }}
             >
                 <DialogTitle>Keyboard Shortcuts</DialogTitle>
-                <DialogContent sx={{mt:4}}>
-<Grid container spacing={3}>
+                <DialogContent sx={{ mt: 4 }}>
+                    <Grid container spacing={3}>
 
-</Grid>
+                    </Grid>
                 </DialogContent>
             </Dialog>
         </>
