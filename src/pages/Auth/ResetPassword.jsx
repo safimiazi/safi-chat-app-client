fb3bca1fca3f3a5fb7f95f690b1c6f019611ad8a
safimiazi <mohibullamiazi@gmail.com>
2023-12-26 22:47:57 +0600

    reset pass input

--- src/pages/Auth/ResetPassword.jsx
+++ src/pages/Auth/ResetPassword.jsx
@@ -1,10 +1,19 @@
+import { Stack, Typography } from '@mui/material';
 import React from 'react';
 
 const ResetPassword = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Stack spacing={2} sx={{mb: 5, position: "relative"}}>
+<Typography variant='h3' paragraph>
+Forget Your Password?
+</Typography>
+<Typography sx={{color: "text.secondary", mb: 5}}>
+Please enter the emeil address associated with your account and we 
+will email you a link to reset your password.
+</Typography>
+            </Stack>
+        </>
     );
 };
 
