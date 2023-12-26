351e7e1c13053c31c656d4ca6260e76d5f215c87
safimiazi <mohibullamiazi@gmail.com>
2023-12-26 22:09:55 +0600

    yup added

--- src/Sections/Auth/RegisterForm.jsx
+++ src/Sections/Auth/RegisterForm.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as Yup from 'yup';
 
 const RegisterForm = () => {
     const [showPassword, setShowPassword] = useState(false);
