7c227dd2035e842e44d5f3b798a45d5b75614b20
safimiazi <mohibullamiazi@gmail.com>
2023-12-26 23:56:15 +0600

    typogray added

--- src/pages/Auth/NewPassword.jsx
+++ src/pages/Auth/NewPassword.jsx
@@ -1,10 +1,19 @@
+import { Stack, Typography } from '@mui/material';
 import React from 'react';
 
 const NewPassword = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+           <Stack spacing={2} sx={{mb:5, position: "relative"}}>
+           <Typography variant='h3' paragraph>
+                    Reset Password?
+                </Typography>
+                <Typography sx={{ color: "text.secondary", mb: 5 }}>
+                    Please your new password
+                </Typography>
+            </Stack> 
+            {/* new password form */}
+        </>
     );
 };
 
