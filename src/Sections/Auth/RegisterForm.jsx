f3673cd366aa67b23c7cc325535bfd6548a6b624
safimiazi <mohibullamiazi@gmail.com>
2023-12-26 22:08:43 +0600

    take some code another component

--- src/Sections/Auth/RegisterForm.jsx
+++ src/Sections/Auth/RegisterForm.jsx
@@ -1,6 +1,38 @@
 import React from 'react';
 
 const RegisterForm = () => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const LoginSchema = Yup.object().shape({
+        email: Yup.string().required("Email is required").email("Email must be a valid email address"),
+        password: Yup.string().required("password is required"),
+    });
+
+    const defaultValues = {
+        email: "demo@safichat.com",
+        password: "demo1234"
+    }
+
+    const methods = useForm({
+        resolver: yupResolver(LoginSchema),
+        defaultValues,
+    });
+
+    const { reset, setError, handleSubmit, formState: { errors, isSubmitting, isSubmittingSuccessful } } = methods;
+
+    const onSubmit = async (data) => {
+        try {
+            //submit data to backend
+        }
+        catch (error) {
+            console.log(error);
+            reset();
+            setError("afterSubmit", {
+                ...error,
+                message: error.message,
+            })
+        }
+    }
     return (
         <div>
             
