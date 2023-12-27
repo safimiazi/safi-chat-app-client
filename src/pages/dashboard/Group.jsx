a7624b15ab4e1365de5234db09311ac2e67535cb
safimiazi <mohibullamiazi@gmail.com>
2023-12-27 21:00:26 +0600

    group section added

--- src/pages/dashboard/Group.jsx
+++ src/pages/dashboard/Group.jsx
@@ -1,11 +1,20 @@
-import { Stack } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import React from 'react';
 
 const Group = () => {
     return (
         <>
-            <Stack>
-                
+            <Stack direction={"row"} sx={{ width: "100%" }}>
+                {/* Left */}
+                <Box sx={{
+                    height: "100vh",
+                    backgroundColor: (theme) => theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background,
+                    width: 320,
+                    boxShadow: "0px 0px 2px rgba(0,0, 0, 0.25)"
+                }}>
+
+                </Box>
+                {/* Right */}
             </Stack>
         </>
     );
