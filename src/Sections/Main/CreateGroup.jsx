ebc9e52eb8b75262290d078db8874026febc6140
safimiazi <mohibullamiazi@gmail.com>
2023-12-29 20:40:18 +0600

    padding added

--- src/Sections/Main/CreateGroup.jsx
+++ src/Sections/Main/CreateGroup.jsx
@@ -1,12 +1,16 @@
 import { Dialog, Slide } from '@mui/material';
 import React from 'react';
+
+
+//Todo => create a reuseable component
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
   });
 const CreateGroup = ({open, handleClose}) => {
     return (
-        <Dialog fullWidth maxWidth="xs" open={open} TransitionComponent={Transition}>
+        <Dialog fullWidth maxWidth="xs" open={open} TransitionComponent={Transition} keepMounted sx={{p:4}}>
             {/* tltle */}
+            {/* content */}
         </Dialog>
     );
 };
