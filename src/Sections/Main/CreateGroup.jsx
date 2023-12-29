a5a84199133546a77085d7b381650a93223bcba7
safimiazi <mohibullamiazi@gmail.com>
2023-12-29 21:06:09 +0600

    NewGroupSchema

--- src/Sections/Main/CreateGroup.jsx
+++ src/Sections/Main/CreateGroup.jsx
@@ -1,11 +1,17 @@
 import { Dialog, DialogContent, DialogTitle, Slide } from '@mui/material';
 import React from 'react';
-
+import * as Yup from 'yup';
 
 //Todo => create a reuseable component
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
   });
+
+  const CreateGroupForm = ({}) => {
+    const NewGroupSchema = Yup.object().shape({
+        
+    })
+  }
 const CreateGroup = ({open, handleClose}) => {
     return (
         <Dialog fullWidth maxWidth="xs" open={open} TransitionComponent={Transition} keepMounted sx={{p:4}}>
@@ -13,7 +19,7 @@ const CreateGroup = ({open, handleClose}) => {
             <DialogTitle>Create New Group</DialogTitle>
             {/* content */}
             <DialogContent>
-                
+                {/* form */}
             </DialogContent>
         </Dialog>
     );
