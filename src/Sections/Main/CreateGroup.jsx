0d4f03662097ffb8b47de349ec7d102febb50671
safimiazi <mohibullamiazi@gmail.com>
2023-12-29 20:36:14 +0600

    transtion added

--- src/Sections/Main/CreateGroup.jsx
+++ src/Sections/Main/CreateGroup.jsx
@@ -1,9 +1,11 @@
-import { Dialog } from '@mui/material';
+import { Dialog, Slide } from '@mui/material';
 import React from 'react';
-
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
 const CreateGroup = ({open, handleClose}) => {
     return (
-        <Dialog fullWidth maxWidth="xs" open={open}>
+        <Dialog fullWidth maxWidth="xs" open={open} TransitionComponent={Transition}>
             
         </Dialog>
     );
