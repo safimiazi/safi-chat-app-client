ec76cedff79a55b6f93eb20569d093a23d4e553e
safimiazi <mohibullamiazi@gmail.com>
2023-12-30 19:14:59 +0600

    typograpy added

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, Stack, useTheme } from '@mui/material';
+import { Box, IconButton, Stack, Typography, useTheme } from '@mui/material';
 import { CaretLeft } from 'phosphor-react';
 import React from 'react';
 
@@ -10,11 +10,12 @@ const Profile = () => {
                 <Box sx={{ height: "100vh", backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background, width: 320, boxShadow: "0px 0px 2px rgba(0,0,0, 0.25)", }}>
                     <Stack p={4} spacing={5}>
                         {/* Header */}
-<Stack direction={"row"} alignItems={"center"} spacing={3}>
-<IconButton>
-    <CaretLeft/>
-</IconButton>
-</Stack>
+                        <Stack direction={"row"} alignItems={"center"} spacing={3}>
+                            <IconButton>
+                                <CaretLeft />
+                            </IconButton>
+                            <Typography></Typography>
+                        </Stack>
                     </Stack>
                 </Box>
             </Stack>
