cee1c6da3f734cf50233916e05ee66160e794a74
safimiazi <mohibullamiazi@gmail.com>
2023-12-30 18:53:06 +0600

    box shadow added

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
     return (
         <>
             <Stack direction={"row"} sx={{ width: "100%" }}>
-                <Box sx={{ height: "100vh", backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background, width: 320,  }}>
+                <Box sx={{ height: "100vh", backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background, width: 320, boxShadow: "0px 0px 2px rgba(0,0,0, 0.25)", }}>
 
                 </Box>
             </Stack>
