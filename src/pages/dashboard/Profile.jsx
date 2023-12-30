638973e465c96c68c9ba32f01ae9834ffe37ae0b
safimiazi <mohibullamiazi@gmail.com>
2023-12-30 19:11:36 +0600

    direction added

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -7,10 +7,12 @@ const Profile = () => {
         <>
             <Stack direction={"row"} sx={{ width: "100%" }}>
                 <Box sx={{ height: "100vh", backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background, width: 320, boxShadow: "0px 0px 2px rgba(0,0,0, 0.25)", }}>
-<Stack p={4} spacing={5}>
-{/* Header */}
+                    <Stack p={4} spacing={5}>
+                        {/* Header */}
+<Stack direction={"row"}>
 
 </Stack>
+                    </Stack>
                 </Box>
             </Stack>
         </>
