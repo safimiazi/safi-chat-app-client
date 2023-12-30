64e3e1a4923b4af558b695a616f3460cdeee0e27
safimiazi <mohibullamiazi@gmail.com>
2023-12-30 18:44:43 +0600

    theme added in the profile page

--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,11 +1,15 @@
-import { Stack } from '@mui/material';
+import { Box, Stack, useTheme } from '@mui/material';
+import { Theme } from 'emoji-picker-react';
 import React from 'react';
 
 const Profile = () => {
+    const theme = useTheme()
     return (
         <>
             <Stack direction={"row"} sx={{width: "100%"}}>
+<Box sx={{height: "100vh", backgroundColor: theme.palette.mode === "light"}}>
 
+</Box>
             </Stack>
         </>
     );
