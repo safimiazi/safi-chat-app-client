4d106d5b4fddb2060ce18f6e58a40edb36f43f6a
safimiazi <mohibullamiazi@gmail.com>
2024-01-13 16:49:31 +0600

    typography added and some text added

--- src/pages/Auth/Verify.jsx
+++ src/pages/Auth/Verify.jsx
@@ -1,10 +1,11 @@
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import React from 'react';
 
 const Verify = () => {
     return (
         <>
-           <Stack>
+           <Stack spacing={2} sx={{mb:5, position: "relative"}}>
+            <Typography>Please Verify OTP</Typography>
             </Stack> 
         </>
     );
