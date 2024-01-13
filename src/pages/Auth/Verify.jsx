8a7942afae06e74ea4457d443e6be9a0bbbb1bdf
safimiazi <mohibullamiazi@gmail.com>
2024-01-13 16:47:42 +0600

    some data input and apply for verify

---
+++ src/pages/Auth/Verify.jsx
@@ -0,0 +1,13 @@
+import { Stack } from '@mui/material';
+import React from 'react';
+
+const Verify = () => {
+    return (
+        <>
+           <Stack>
+            </Stack> 
+        </>
+    );
+};
+
+export default Verify;
