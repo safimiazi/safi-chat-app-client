b729326d61d94f649c739a5ad2a6b146b58e41e6
safimiazi <mohibullamiazi@gmail.com>
2024-01-16 18:36:38 +0600

    feat: Add socket event listeners for friend requests

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -2,18 +2,61 @@ import { useTheme } from "@emotion/react";
 import { Navigate, Outlet } from "react-router-dom";
 import Stack from '@mui/material/Stack';
 import SideBar from "../../pages/dashboard/SideBar";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { connectSocket, socket } from "../../Socket";
+import { showSnackbar } from "../../redux/slices/Apps";
 
 
 
 const DashboardLayout = () => {
-  const {isLoggedIn} = useSelector((state) => state.auth)
+  const dispatch = useDispatch()
+  const { isLoggedIn } = useSelector((state) => state.auth)
   const theme = useTheme()
   console.log(theme);
-  
-if(!isLoggedIn) {
-  return <Navigate to="/auth/login"/>
-}
+  const user_id = window.localStorage.getItem("user_id");
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      window.onload = function () {
+        if (!window.location.hash) {
+          window.location = window.location + "#loaded";
+          window.location.reload();
+        }
+      }
+
+      window.reload();
+
+      if (!socket) {
+        connectSocket(user_id)
+      }
+
+      //"new_friend_request"
+
+      socket.on("new_friend_request", (data) => {
+        dispatch(showSnackbar({ severity: "success", message: data.message }))
+      });
+      socket.on("request_accepted", (data) => {
+        dispatch(showSnackbar({ severity: "success", message: data.message }))
+      });
+      socket.on("request_sent", (data) => {
+        dispatch(showSnackbar({ severity: "success", message: data.message }))
+      });
+
+    }
+
+    return () => {
+      socket.off("new_friend_request");
+      socket.off("request_accepted");
+      socket.off("request_sent");
+    }
+  },[isLoggedIn, socket])
+
+
+
+  if (!isLoggedIn) {
+    return <Navigate to="/auth/login" />
+  }
 
   return (
     <>
